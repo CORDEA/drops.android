0274c56312d9e930e550716cd2b55986f594d4b4
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-19 14:00:03 +0900

    simplify

--- app/src/main/java/jp/cordea/drops/MainActivity.kt
+++ app/src/main/java/jp/cordea/drops/MainActivity.kt
@@ -3,14 +3,11 @@ package jp.cordea.drops
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import jp.cordea.drops.databinding.MainActivityBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = MainActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
+        setContentView(R.layout.main_activity)
     }
 }
