f9e5f12bd081fe1c2b5d82dd8107a30e6761ddac
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-19 14:17:08 +0900

    add new fragment

---
+++ ui/login/src/main/java/jp/cordea/drops/ui/login/LoginFragment.kt
@@ -0,0 +1,16 @@
+package jp.cordea.drops.ui.login
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.login_fragment) {
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
