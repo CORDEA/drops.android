afd62222aefe11e1bf7a6ccc5b5366bf68b3d473
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-22 13:12:53 +0900

    bind

--- ui/login/src/main/java/jp/cordea/drops/ui/login/SignUpFragment.kt
+++ ui/login/src/main/java/jp/cordea/drops/ui/login/SignUpFragment.kt
@@ -1,10 +1,20 @@
 package jp.cordea.drops.ui.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import jp.cordea.drops.ui.login.databinding.SignUpFragmentBinding
 
 @AndroidEntryPoint
 class SignUpFragment : Fragment(R.layout.sign_up_fragment) {
-    private val viewModel: WelcomeViewModel by viewModels()
+    private val viewModel: SignUpViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = SignUpFragmentBinding.bind(view)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+    }
 }
--- ui/login/src/main/java/jp/cordea/drops/ui/login/SignUpViewModel.kt
+++ ui/login/src/main/java/jp/cordea/drops/ui/login/SignUpViewModel.kt
@@ -1,6 +1,12 @@
 package jp.cordea.drops.ui.login
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SignUpViewModel @ViewModelInject constructor() : ViewModel()
+class SignUpViewModel @ViewModelInject constructor() : ViewModel() {
+    val code = MutableLiveData("")
+
+    fun onSignUpClick() {
+    }
+}
