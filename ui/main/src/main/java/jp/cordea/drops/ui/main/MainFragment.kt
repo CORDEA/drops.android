0503478c92569e7eb98fed58c30896865f9b8268
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-25 23:29:27 +0900

    change view size

--- ui/main/src/main/java/jp/cordea/drops/ui/main/MainFragment.kt
+++ ui/main/src/main/java/jp/cordea/drops/ui/main/MainFragment.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.view.ContextThemeWrapper
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,11 +38,6 @@ class MainFragment : Fragment() {
         binding.recyclerView.adapter = adapter
         binding.recyclerView.addItemDecoration(MainItemDecoration(requireContext()))
         binding.recyclerView.layoutManager =
-            object : GridLayoutManager(requireContext(), 3, HORIZONTAL, false) {
-                override fun checkLayoutParams(lp: RecyclerView.LayoutParams?): Boolean {
-                    lp?.width = (width * 0.5f).toInt()
-                    return true
-                }
-            }
+            GridLayoutManager(requireContext(), 3, GridLayoutManager.HORIZONTAL, false)
     }
 }
