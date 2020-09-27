c71779c9dcbd8d51a6afede1f4625a87237a0683
Yoshihiro Tanaka <contact@cordea.jp>
2020-09-27 20:56:17 +0900

    implement again for animation

--- ui/shared/src/main/java/jp/cordea/drops/ui/ExpandableToolbar.kt
+++ ui/shared/src/main/java/jp/cordea/drops/ui/ExpandableToolbar.kt
@@ -1,5 +1,7 @@
 package jp.cordea.drops.ui
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -10,23 +12,37 @@ import androidx.annotation.LayoutRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.isVisible
 import jp.cordea.drops.ui.databinding.ExpandableToolbarBinding
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
 
 class ExpandableToolbar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : FrameLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior, CoroutineScope {
     private val binding: ExpandableToolbarBinding =
         ExpandableToolbarBinding.inflate(LayoutInflater.from(context), this, true)
+    private val behavior = Behavior(context)
 
-    private var currentMode = Mode.COLLAPSED
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main.immediate
+
+    private val nextState = Channel<State>()
+    private var currentState = State.COLLAPSED
 
     init {
         binding.navigationIcon.setOnClickListener {
-            switch(if (currentMode == Mode.COLLAPSED) Mode.EXPANDED_NAVIGATION else Mode.COLLAPSED)
+            nextState.offer(
+                if (currentState == State.COLLAPSED) State.EXPANDED_NAVIGATION else State.COLLAPSED
+            )
         }
         binding.menuIcon.setOnClickListener {
-            switch(if (currentMode == Mode.COLLAPSED) Mode.EXPANDED_MENU else Mode.COLLAPSED)
+            nextState.offer(
+                if (currentState == State.COLLAPSED) State.EXPANDED_MENU else State.COLLAPSED
+            )
         }
     }
 
@@ -40,73 +56,114 @@ class ExpandableToolbar @JvmOverloads constructor(
         binding.menuIcon.isVisible = true
     }
 
-    private fun switch(next: Mode) {
-        if (currentMode == next) {
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        job = SupervisorJob()
+        launch {
+            nextState.consumeEach {
+                switch(it)
+            }
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        job.cancel()
+        super.onDetachedFromWindow()
+    }
+
+    override fun getBehavior(): CoordinatorLayout.Behavior<*> = behavior
+
+    private suspend fun switch(next: State) {
+        if (currentState == next) {
             return
         }
         val enableMenu = binding.menuIcon.isVisible
         when (next) {
-            Mode.EXPANDED_NAVIGATION -> {
+            State.EXPANDED_NAVIGATION -> {
                 if (enableMenu) {
                     binding.menu.isVisible = false
                 }
                 binding.navigationMenu.isVisible = true
+                behavior.slideDown(this)
             }
-            Mode.EXPANDED_MENU -> {
+            State.EXPANDED_MENU -> {
                 if (!enableMenu) {
                     throw IllegalArgumentException()
                 }
                 binding.navigationMenu.isVisible = false
                 binding.menu.isVisible = true
             }
-            Mode.COLLAPSED -> {
+            State.COLLAPSED -> {
+                behavior.slideUp(this)
                 binding.navigationMenu.isVisible = false
                 if (enableMenu) {
                     binding.menu.isVisible = false
                 }
             }
         }
-        currentMode = next
+        currentState = next
     }
 
-    private enum class Mode {
+    private fun findFrontLayer(): View? {
+        val parent = parent
+        if (parent !is CoordinatorLayout) {
+            return null
+        }
+        val views = parent.getDependents(this)
+        return views.firstOrNull { it is BackdropFrontLayer }
+    }
+
+    private enum class State {
         EXPANDED_NAVIGATION,
         EXPANDED_MENU,
         COLLAPSED
     }
 
-    class ScrollingViewBehavior(
-        context: Context,
-        attrs: AttributeSet
-    ) : CoordinatorLayout.Behavior<View>(context, attrs) {
-        private val toolbarHeight = context.resources.getDimensionPixelSize(
+    private class Behavior(context: Context) : CoordinatorLayout.Behavior<ExpandableToolbar>() {
+        companion object {
+            private const val DURATION = 200L
+        }
+
+        private val toolbarMinHeight = context.resources.getDimensionPixelSize(
             R.dimen.expandable_toolbar_min_height
         )
 
-        override fun layoutDependsOn(
-            parent: CoordinatorLayout,
-            child: View,
-            dependency: View
-        ): Boolean = dependency is ExpandableToolbar
-
-        override fun onDependentViewChanged(
-            parent: CoordinatorLayout,
-            child: View,
-            dependency: View
-        ): Boolean {
-            if (dependency.height <= 0) {
-                return false
-            }
-            val diff = dependency.height - toolbarHeight
+        suspend fun slideDown(toolbar: ExpandableToolbar) {
+            val layer = toolbar.findFrontLayer() ?: return
+            val diff = toolbar.height - toolbarMinHeight
             if (diff <= 0) {
-                if (child.translationY > 0) {
-                    child.translationY = 0f
-                    return true
-                }
-                return false
+                return
             }
-            child.translationY = diff.toFloat()
-            return true
+            return animateTo(layer, diff.toFloat())
+        }
+
+        suspend fun slideUp(toolbar: ExpandableToolbar) {
+            val layer = toolbar.findFrontLayer() ?: return
+            return animateTo(layer, 0f)
+        }
+
+        private suspend fun animateTo(view: View, to: Float) {
+            val animator = view.animate()
+            return suspendCancellableCoroutine {
+                it.invokeOnCancellation { animator.cancel() }
+                animator
+                    .translationY(to)
+                    .setDuration(DURATION)
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            if (it.isActive) {
+                                it.resume(Unit)
+                            }
+                        }
+
+                        override fun onAnimationCancel(animation: Animator?) {
+                            if (it.isActive) {
+                                it.resume(Unit)
+                            }
+                        }
+                    })
+                    .start()
+            }
         }
     }
 }
