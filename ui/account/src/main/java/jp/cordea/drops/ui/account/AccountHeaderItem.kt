a6d1a098c861f0e465f46f03e7d0562ee8334ccc
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-06 09:36:15 +0900

    define item

---
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountHeaderItem.kt
@@ -0,0 +1,20 @@
+package jp.cordea.drops.ui.account
+
+import android.view.View
+import com.xwray.groupie.viewbinding.BindableItem
+import jp.cordea.drops.ui.account.databinding.AccountHeaderItemBinding
+
+class AccountHeaderItemViewModel
+
+class AccountHeaderItem(
+    private val viewModel: AccountHeaderItemViewModel
+) : BindableItem<AccountHeaderItemBinding>() {
+    override fun getLayout(): Int = R.layout.account_header_item
+
+    override fun bind(viewBinding: AccountHeaderItemBinding, position: Int) {
+        viewBinding.viewModel = viewModel
+    }
+
+    override fun initializeViewBinding(view: View): AccountHeaderItemBinding =
+        AccountHeaderItemBinding.bind(view)
+}
