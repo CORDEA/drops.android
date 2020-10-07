39a3b660238f2804b09afdddc980824b81adf813
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-07 09:55:27 +0900

    add viewmodel

--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
@@ -3,11 +3,14 @@ package jp.cordea.drops.ui.account
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.account.databinding.AccountFragmentBinding
 
 @AndroidEntryPoint
 class AccountFragment : Fragment(R.layout.account_fragment) {
+    private val viewModel: AccountViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = AccountFragmentBinding.bind(view)
---
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountViewModel.kt
@@ -0,0 +1,7 @@
+package jp.cordea.drops.ui.account
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel @ViewModelInject constructor() : ViewModel() {
+}
