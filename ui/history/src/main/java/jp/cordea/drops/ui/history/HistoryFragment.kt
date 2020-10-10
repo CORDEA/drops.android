5e3a6c4460819476ceb1a41288f15e48298095e6
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-10 13:59:05 +0900

    handle click events

--- ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryFragment.kt
+++ ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.bindNavigationMenu
 import jp.cordea.drops.ui.history.databinding.HistoryFragmentBinding
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HistoryFragment : Fragment(R.layout.history_fragment) {
+    @Inject
+    lateinit var navigator: HistoryNavigator
+
     private val viewModel: HistoryViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -18,5 +24,18 @@ class HistoryFragment : Fragment(R.layout.history_fragment) {
         binding.toolbar.setNavigationView(R.layout.navigation_menu)
         binding.toolbar.bindNavigationMenu(viewModel)
         binding.lifecycleOwner = viewLifecycleOwner
+
+        lifecycleScope.launch {
+            for (event in viewModel.onEvent) {
+                handleEvent(event)
+            }
+        }
+    }
+
+    private fun handleEvent(event: HistoryViewModel.Event) {
+        when (event) {
+            HistoryViewModel.Event.ClickedCatalog -> navigator.navigateToCatalog()
+            HistoryViewModel.Event.ClickedAccount -> navigator.navigateToAccount()
+        }
     }
 }
--- ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryViewModel.kt
+++ ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryViewModel.kt
@@ -3,14 +3,26 @@ package jp.cordea.drops.ui.history
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import jp.cordea.drops.ui.NavigationMenuBindable
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 
 class HistoryViewModel @ViewModelInject constructor() : ViewModel(), NavigationMenuBindable {
+    private val _onEvent = Channel<Event>()
+    val onEvent: ReceiveChannel<Event> get() = _onEvent
+
     override fun onCatalogClick() {
+        _onEvent.offer(Event.ClickedCatalog)
     }
 
     override fun onHistoryClick() {
     }
 
     override fun onAccountClick() {
+        _onEvent.offer(Event.ClickedAccount)
+    }
+
+    sealed class Event {
+        object ClickedCatalog : Event()
+        object ClickedAccount : Event()
     }
 }
