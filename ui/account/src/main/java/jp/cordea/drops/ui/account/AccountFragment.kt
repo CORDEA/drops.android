073419a09dcb3d04ac2a0c65ac1e1ecb5ac2ec19
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-10 14:09:17 +0900

    handle click events

--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.account.databinding.AccountFragmentBinding
 import jp.cordea.drops.ui.bindNavigationMenu
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AccountFragment : Fragment(R.layout.account_fragment) {
+    @Inject
+    lateinit var navigator: AccountNavigator
+
     private val viewModel: AccountViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -18,5 +24,18 @@ class AccountFragment : Fragment(R.layout.account_fragment) {
         binding.toolbar.setNavigationView(R.layout.navigation_menu)
         binding.toolbar.bindNavigationMenu(viewModel)
         binding.lifecycleOwner = viewLifecycleOwner
+
+        lifecycleScope.launch {
+            for (event in viewModel.onEvent) {
+                handleEvent(event)
+            }
+        }
+    }
+
+    private fun handleEvent(event: AccountViewModel.Event) {
+        when (event) {
+            AccountViewModel.Event.ClickedCatalog -> navigator.navigateToCatalog()
+            AccountViewModel.Event.ClickedHistory -> navigator.navigateToHistory()
+        }
     }
 }
--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountViewModel.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountViewModel.kt
@@ -3,14 +3,26 @@ package jp.cordea.drops.ui.account
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import jp.cordea.drops.ui.NavigationMenuBindable
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 
 class AccountViewModel @ViewModelInject constructor() : ViewModel(), NavigationMenuBindable {
+    private val _onEvent = Channel<Event>()
+    val onEvent: ReceiveChannel<Event> get() = _onEvent
+
     override fun onCatalogClick() {
+        _onEvent.offer(Event.ClickedCatalog)
     }
 
     override fun onHistoryClick() {
+        _onEvent.offer(Event.ClickedHistory)
     }
 
     override fun onAccountClick() {
+    }
+
+    sealed class Event {
+        object ClickedCatalog : Event()
+        object ClickedHistory : Event()
     }
 }
