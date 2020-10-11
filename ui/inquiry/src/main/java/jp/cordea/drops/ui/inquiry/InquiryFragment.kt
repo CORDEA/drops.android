c532cd65c9dd37891b85d8fb594a8fd0eafd7f96
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-11 09:31:04 +0900

    add viewmodel

--- ui/inquiry/src/main/java/jp/cordea/drops/ui/inquiry/InquiryFragment.kt
+++ ui/inquiry/src/main/java/jp/cordea/drops/ui/inquiry/InquiryFragment.kt
@@ -3,11 +3,14 @@ package jp.cordea.drops.ui.inquiry
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.inquiry.databinding.InquiryFragmentBinding
 
 @AndroidEntryPoint
 class InquiryFragment : Fragment(R.layout.inquiry_fragment) {
+    private val viewModel: InquiryViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = InquiryFragmentBinding.bind(view)
---
+++ ui/inquiry/src/main/java/jp/cordea/drops/ui/inquiry/InquiryViewModel.kt
@@ -0,0 +1,6 @@
+package jp.cordea.drops.ui.inquiry
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class InquiryViewModel @ViewModelInject constructor() : ViewModel()
