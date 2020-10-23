4379e08b9ed255a32565c09d2e909a89d203945b
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-23 09:56:11 +0900

    define module

---
+++ infra/src/main/java/jp/cordea/drops/infra/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package jp.cordea.drops.infra
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import jp.cordea.drops.domain.repository.AchievementRepository
+import jp.cordea.drops.domain.repository.ItemRepository
+import jp.cordea.drops.domain.repository.UserRepository
+
+@Module
+@InstallIn(ApplicationComponent::class)
+internal interface RepositoryModule {
+    @Binds
+    fun bindItemRepository(impl: ItemRepositoryImpl): ItemRepository
+
+    @Binds
+    fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
+
+    @Binds
+    fun bindAchievementRepository(impl: AchievementRepositoryImpl): AchievementRepository
+}
