ef78d965034f7940bd6b8ec2185bd8e2a2d6a85a
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-23 09:46:39 +0900

    update response

--- domain/src/main/java/jp/cordea/drops/domain/Order.kt
+++ domain/src/main/java/jp/cordea/drops/domain/Order.kt
@@ -1,15 +1,26 @@
 package jp.cordea.drops.domain
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 class Order(
     val id: String,
     val status: Status,
     val items: List<Item>,
-    val isCancelable: Boolean
+    @Json(name = "is_cancelable") val isCancelable: Boolean
 ) {
     enum class Status {
+        @Json(name = "confirmed")
         CONFIRMED,
+
+        @Json(name = "shipped")
         SHIPPED,
+
+        @Json(name = "delivered")
         DELIVERED,
+
+        @Json(name = "canceled")
         CANCELED
     }
 }
