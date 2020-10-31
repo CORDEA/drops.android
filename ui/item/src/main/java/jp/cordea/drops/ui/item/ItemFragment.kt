83624b54585ff2180273373de60dc3a898370738
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-31 09:52:26 +0900

    pass item

--- ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import jp.cordea.drops.ui.item.databinding.ItemFragmentBinding
 
 class ItemFragment : Fragment(R.layout.item_fragment) {
     private val viewModel: ItemViewModel by viewModels()
 
+    private val args: ItemFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = ItemFragmentBinding.bind(view)
         binding.lifecycleOwner = viewLifecycleOwner
+        viewModel.init(args.item)
     }
 }
--- ui/item/src/main/java/jp/cordea/drops/ui/item/ItemViewModel.kt
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemViewModel.kt
@@ -2,6 +2,10 @@ package jp.cordea.drops.ui.item
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import jp.cordea.drops.domain.Item
 
 class ItemViewModel @ViewModelInject constructor() : ViewModel() {
+
+    fun init(item: Item) {
+    }
 }
