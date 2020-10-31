23b825085b36ed241253c144b1ecb5a8b0be8c6d
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-31 14:08:24 +0900

    add items

--- ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.item.databinding.ItemFragmentBinding
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class ItemFragment : Fragment(R.layout.item_fragment) {
@@ -19,5 +24,13 @@ class ItemFragment : Fragment(R.layout.item_fragment) {
         val binding = ItemFragmentBinding.bind(view)
         binding.lifecycleOwner = viewLifecycleOwner
         viewModel.init(args.item)
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = adapter
+        viewModel.items
+            .onEach { list ->
+                adapter.updateAsync(list.map { ListItem(it) })
+            }
+            .launchIn(lifecycleScope)
     }
 }
--- ui/item/src/main/java/jp/cordea/drops/ui/item/ItemViewModel.kt
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemViewModel.kt
@@ -1,11 +1,53 @@
 package jp.cordea.drops.ui.item
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import jp.cordea.drops.domain.Item
+import jp.cordea.drops.ui.ResourceProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class ItemViewModel @ViewModelInject constructor() : ViewModel() {
+class ItemViewModel @ViewModelInject constructor(
+    private val resourceProvider: ResourceProvider
+) : ViewModel() {
+    private val _items = MutableStateFlow(emptyList<ListItemViewModel>())
+    val items: StateFlow<List<ListItemViewModel>> get() = _items
 
     fun init(item: Item) {
+        _items.value = listOf(
+            ListItemViewModel(
+                resourceProvider.getString(R.string.label_item_height),
+                resourceProvider.getString(
+                    R.string.item_body_format,
+                    item.itemHeight,
+                    item.itemDimensionsUnit
+                )
+            ),
+            ListItemViewModel(
+                resourceProvider.getString(R.string.label_item_width),
+                resourceProvider.getString(
+                    R.string.item_body_format,
+                    item.itemWidth,
+                    item.itemDimensionsUnit
+                )
+            ),
+            ListItemViewModel(
+                resourceProvider.getString(R.string.label_item_length),
+                resourceProvider.getString(
+                    R.string.item_body_format,
+                    item.itemLength,
+                    item.itemDimensionsUnit
+                )
+            ),
+            ListItemViewModel(
+                resourceProvider.getString(R.string.label_item_weight),
+                resourceProvider.getString(
+                    R.string.item_body_format,
+                    item.itemWeight,
+                    item.itemWeightUnit
+                )
+            )
+        )
     }
 }
