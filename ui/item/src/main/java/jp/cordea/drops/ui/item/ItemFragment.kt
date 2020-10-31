98957600b0a28e71f565c001f4253b450e621da0
Yoshihiro Tanaka <contact@cordea.jp>
2020-10-31 09:50:17 +0900

    add viewmodel

--- ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemFragment.kt
@@ -3,9 +3,12 @@ package jp.cordea.drops.ui.item
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import jp.cordea.drops.ui.item.databinding.ItemFragmentBinding
 
 class ItemFragment : Fragment(R.layout.item_fragment) {
+    private val viewModel: ItemViewModel by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = ItemFragmentBinding.bind(view)
---
+++ ui/item/src/main/java/jp/cordea/drops/ui/item/ItemViewModel.kt
@@ -0,0 +1,7 @@
+package jp.cordea.drops.ui.item
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class ItemViewModel @ViewModelInject constructor() : ViewModel() {
+}
