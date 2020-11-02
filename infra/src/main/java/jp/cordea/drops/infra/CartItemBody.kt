a39546ea6687b3720d1d011045826304d4630c8d
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-02 15:01:31 +0900

    generate adapter

--- infra/src/main/java/jp/cordea/drops/infra/CartItemBody.kt
+++ infra/src/main/java/jp/cordea/drops/infra/CartItemBody.kt
@@ -1,5 +1,8 @@
 package jp.cordea.drops.infra
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 internal class CartItemBody(
     val id: Long
 )
--- infra/src/main/java/jp/cordea/drops/infra/SignUpBody.kt
+++ infra/src/main/java/jp/cordea/drops/infra/SignUpBody.kt
@@ -1,7 +1,9 @@
 package jp.cordea.drops.infra
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 internal class SignUpBody(
     @Json(name = "email_address") val emailAddress: String,
     val password: String
