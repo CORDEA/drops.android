a31e0b1fa5e4d22f968bfc94c6bd54f737071b93
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-03 10:14:25 +0900

    rename

--- ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryCompletedItem.kt
+++ ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryCompletedItem.kt
@@ -3,19 +3,19 @@ package jp.cordea.drops.ui.history
 import android.view.View
 import com.xwray.groupie.Item
 import com.xwray.groupie.viewbinding.BindableItem
-import jp.cordea.drops.ui.history.databinding.HistoryItemBinding
+import jp.cordea.drops.ui.history.databinding.HistoryCompletedItemBinding
 
 class HistoryCompletedItem(
     private val viewModel: HistoryCompletedItemViewModel
-) : BindableItem<HistoryItemBinding>() {
-    override fun getLayout(): Int = R.layout.history_item
+) : BindableItem<HistoryCompletedItemBinding>() {
+    override fun getLayout(): Int = R.layout.history_completed_item
 
-    override fun bind(viewBinding: HistoryItemBinding, position: Int) {
+    override fun bind(viewBinding: HistoryCompletedItemBinding, position: Int) {
         viewBinding.viewModel = viewModel
     }
 
-    override fun initializeViewBinding(view: View): HistoryItemBinding =
-        HistoryItemBinding.bind(view)
+    override fun initializeViewBinding(view: View): HistoryCompletedItemBinding =
+        HistoryCompletedItemBinding.bind(view)
 
     override fun isSameAs(other: Item<*>): Boolean =
         (other as? HistoryCompletedItem)?.viewModel?.id == viewModel.id
--- ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryInProgressItem.kt
+++ ui/history/src/main/java/jp/cordea/drops/ui/history/HistoryInProgressItem.kt
@@ -3,19 +3,19 @@ package jp.cordea.drops.ui.history
 import android.view.View
 import com.xwray.groupie.Item
 import com.xwray.groupie.viewbinding.BindableItem
-import jp.cordea.drops.ui.history.databinding.HistoryStatusItemBinding
+import jp.cordea.drops.ui.history.databinding.HistoryInProgressItemBinding
 
 class HistoryInProgressItem(
     private val viewModel: HistoryInProgressItemViewModel
-) : BindableItem<HistoryStatusItemBinding>() {
-    override fun getLayout(): Int = R.layout.history_status_item
+) : BindableItem<HistoryInProgressItemBinding>() {
+    override fun getLayout(): Int = R.layout.history_in_progress_item
 
-    override fun bind(viewBinding: HistoryStatusItemBinding, position: Int) {
+    override fun bind(viewBinding: HistoryInProgressItemBinding, position: Int) {
         viewBinding.viewModel = viewModel
     }
 
-    override fun initializeViewBinding(view: View): HistoryStatusItemBinding =
-        HistoryStatusItemBinding.bind(view)
+    override fun initializeViewBinding(view: View): HistoryInProgressItemBinding =
+        HistoryInProgressItemBinding.bind(view)
 
     override fun isSameAs(other: Item<*>): Boolean =
         (other as? HistoryInProgressItem)?.viewModel?.id == viewModel.id
