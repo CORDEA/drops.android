e2e718e517b46b7bd07c5e9463a760bac04071ad
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-04 10:30:09 +0900

    update response

--- domain/src/main/java/jp/cordea/drops/domain/Order.kt
+++ domain/src/main/java/jp/cordea/drops/domain/Order.kt
@@ -2,13 +2,18 @@ package jp.cordea.drops.domain
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import java.time.LocalDateTime
 
 @JsonClass(generateAdapter = true)
 class Order(
     val id: String,
     val status: Status,
     val items: List<Item>,
-    @Json(name = "is_cancelable") val isCancelable: Boolean
+    @Json(name = "is_cancelable") val isCancelable: Boolean,
+    @Json(name = "ordered_at") val orderedAt: LocalDateTime,
+    @Json(name = "shipped_at") val shippedAt: LocalDateTime?,
+    @Json(name = "delivered_at") val deliveredAt: LocalDateTime?,
+    @Json(name = "canceled_at") val canceledAt: LocalDateTime?
 ) {
     enum class Status {
         @Json(name = "confirmed")
