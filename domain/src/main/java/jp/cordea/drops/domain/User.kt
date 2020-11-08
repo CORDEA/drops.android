90d94947b273d5ff3b7ba8317a44cdcb81459fab
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-08 12:01:14 +0900

    update response

--- domain/src/main/java/jp/cordea/drops/domain/User.kt
+++ domain/src/main/java/jp/cordea/drops/domain/User.kt
@@ -3,13 +3,13 @@ package jp.cordea.drops.domain
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import java.time.LocalDateTime
-import java.util.*
 
 @JsonClass(generateAdapter = true)
 class User(
     val id: UserId,
     val name: String,
     val description: String,
+    val imageUrl: String,
     val city: String,
     val rank: Int,
     @Json(name = "created_at") val createdAt: LocalDateTime,
