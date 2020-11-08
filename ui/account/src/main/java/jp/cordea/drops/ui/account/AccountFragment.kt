3fa7425485c5f8769ee192ee172869897ae2c180
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-08 12:11:11 +0900

    display items

--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.drops.ui.account.databinding.AccountFragmentBinding
 import jp.cordea.drops.ui.bindNavigationMenu
 import jp.cordea.drops.ui.navigation.NavGraphDirections
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -25,11 +30,26 @@ class AccountFragment : Fragment(R.layout.account_fragment) {
         binding.toolbar.bindNavigationMenu(viewModel)
         binding.lifecycleOwner = viewLifecycleOwner
 
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = adapter
+
         lifecycleScope.launch {
             for (event in viewModel.onEvent) {
                 handleEvent(event)
             }
         }
+
+        viewModel.items
+            .map { list ->
+                list.map {
+                    when (it) {
+                        is AccountHeaderItemViewModel -> AccountHeaderItem(it)
+                        is AccountListItemViewModel -> AccountListItem(it)
+                    }
+                }
+            }
+            .onEach { adapter.updateAsync(it) }
+            .launchIn(lifecycleScope)
     }
 
     private suspend fun handleEvent(event: AccountViewModel.Event) {
