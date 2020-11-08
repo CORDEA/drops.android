61f67265d58fae89779875b96a8a3eb85ec077f9
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-08 12:25:34 +0900

    load image

--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountHeaderItem.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountHeaderItem.kt
@@ -1,17 +1,28 @@
 package jp.cordea.drops.ui.account
 
 import android.view.View
+import coil.load
+import coil.request.Disposable
 import com.xwray.groupie.Item
 import com.xwray.groupie.viewbinding.BindableItem
+import com.xwray.groupie.viewbinding.GroupieViewHolder
 import jp.cordea.drops.ui.account.databinding.AccountHeaderItemBinding
 
 class AccountHeaderItem(
     private val viewModel: AccountHeaderItemViewModel
 ) : BindableItem<AccountHeaderItemBinding>() {
+    private var disposable: Disposable? = null
+
     override fun getLayout(): Int = R.layout.account_header_item
 
     override fun bind(viewBinding: AccountHeaderItemBinding, position: Int) {
         viewBinding.viewModel = viewModel
+        disposable = viewBinding.imageView.load(viewModel.imageUrl)
+    }
+
+    override fun unbind(viewHolder: GroupieViewHolder<AccountHeaderItemBinding>) {
+        disposable?.dispose()
+        super.unbind(viewHolder)
     }
 
     override fun initializeViewBinding(view: View): AccountHeaderItemBinding =
