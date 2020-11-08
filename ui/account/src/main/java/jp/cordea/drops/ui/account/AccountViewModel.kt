2abf6460a669c847fdd623d34cc98efa248fe7c3
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-08 12:05:51 +0900

    fetch user

--- ui/account/src/main/java/jp/cordea/drops/ui/account/AccountViewModel.kt
+++ ui/account/src/main/java/jp/cordea/drops/ui/account/AccountViewModel.kt
@@ -2,13 +2,32 @@ package jp.cordea.drops.ui.account
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jp.cordea.drops.domain.repository.UserRepository
 import jp.cordea.drops.ui.NavigationMenuBindable
+import jp.cordea.drops.ui.ResourceProvider
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.*
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
-class AccountViewModel @ViewModelInject constructor() : ViewModel(), NavigationMenuBindable {
+class AccountViewModel @ViewModelInject constructor(
+    private val resourceProvider: ResourceProvider,
+    private val repository: UserRepository
+) : ViewModel(), NavigationMenuBindable {
+    private val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
+
     private val _onEvent = Channel<Event>()
     val onEvent: ReceiveChannel<Event> get() = _onEvent
+
+    private val _items = MutableStateFlow(emptyList<AccountItemViewModel>())
+    val items: StateFlow<List<AccountItemViewModel>> get() = _items
+
+    init {
+        refresh()
+    }
 
     override fun onCatalogClick() {
         _onEvent.offer(Event.NavigateToCatalog)
@@ -24,6 +43,34 @@ class AccountViewModel @ViewModelInject constructor() : ViewModel(), NavigationM
 
     override fun onInquiryClick() {
         _onEvent.offer(Event.NavigateToInquiry)
+    }
+
+    private fun refresh() {
+        repository.find()
+            .map {
+                listOf(
+                    AccountHeaderItemViewModel(
+                        it.imageUrl,
+                        it.name,
+                        it.description
+                    ),
+                    AccountListItemViewModel(
+                        resourceProvider.getString(R.string.account_item_rank),
+                        it.rank.toString()
+                    ),
+                    AccountListItemViewModel(
+                        resourceProvider.getString(R.string.account_item_city),
+                        it.city
+                    ),
+                    AccountListItemViewModel(
+                        resourceProvider.getString(R.string.account_item_birthday),
+                        it.birthday.format(formatter)
+                    )
+                )
+            }
+            .flowOn(Dispatchers.IO)
+            .onEach { _items.value = it }
+            .launchIn(viewModelScope)
     }
 
     sealed class Event {
