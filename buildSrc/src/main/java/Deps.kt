156f9c17f598893ff0649cf8c2036070be30b644
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-14 13:29:46 +0900

    prepare for test

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -13,6 +13,7 @@ object Deps {
     val material = "com.google.android.material:material:1.2.1"
     val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.1"
     val lifecycleLivedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
+    val coreTesting = "androidx.arch.core:core-testing:2.1.0"
 
     val navigationSafeArgsGradlePlugin =
         "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
@@ -27,8 +28,9 @@ object Deps {
     val hiltAndroid = "com.google.dagger:hilt-android:2.28.3-alpha"
     val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:2.28.3-alpha"
 
-    val kotlinxCoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7"
-    val kotlinxCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.7"
+    val kotlinxCoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1"
+    val kotlinxCoroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1"
+    val kotlinxCoroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.1"
 
     val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
     val converterMoshi = "com.squareup.retrofit2:converter-moshi:2.9.0"
@@ -42,4 +44,9 @@ object Deps {
     val coil = "io.coil-kt:coil:1.0.0"
 
     val timber = "com.jakewharton.timber:timber:4.7.1"
+
+    val junit = "junit:junit:4.12"
+    val mockk = "io.mockk:mockk:1.10.2"
+    val striktCore = "io.strikt:strikt-core:0.28.0"
+    val striktMockk = "io.strikt:strikt-mockk:0.28.0"
 }
