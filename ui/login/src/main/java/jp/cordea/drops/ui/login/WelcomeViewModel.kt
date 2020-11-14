4eaa19ae0d5642cc8baabc4fb8e21b9e5058ed37
Yoshihiro Tanaka <contact@cordea.jp>
2020-11-14 10:32:12 +0900

    confirm existence of the user

--- ui/login/src/main/java/jp/cordea/drops/ui/login/WelcomeViewModel.kt
+++ ui/login/src/main/java/jp/cordea/drops/ui/login/WelcomeViewModel.kt
@@ -3,10 +3,17 @@ package jp.cordea.drops.ui.login
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import jp.cordea.drops.domain.EmailAddress
+import jp.cordea.drops.domain.repository.UserRepository
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class WelcomeViewModel @ViewModelInject constructor() : ViewModel() {
+class WelcomeViewModel @ViewModelInject constructor(
+    private val repository: UserRepository
+) : ViewModel() {
     private val _onEvent = Channel<Event>()
     val onEvent: ReceiveChannel<Event> = _onEvent
 
@@ -14,7 +21,18 @@ class WelcomeViewModel @ViewModelInject constructor() : ViewModel() {
 
     fun onKnockClick() {
         val email = requireNotNull(emailAddress.value)
-        _onEvent.offer(Event.NavigateToLogIn(email))
+        repository
+            .isUserExists(EmailAddress(email))
+            .onEach { isUserExists ->
+                _onEvent.offer(
+                    if (isUserExists) {
+                        Event.NavigateToLogIn(email)
+                    } else {
+                        Event.NavigateToSignUp(email)
+                    }
+                )
+            }
+            .launchIn(viewModelScope)
     }
 
     sealed class Event {
